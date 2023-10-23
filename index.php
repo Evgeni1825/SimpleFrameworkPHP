8c137262dee4713b165b640294824070fe07cad7
Evgeni1825 <44087970+Evgeni1825@users.noreply.github.com>
2023-10-23 16:40:09 +0300

    Create_Store_for_routes

--- index.php
+++ index.php
@@ -3,14 +3,13 @@
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/vendor/larapack/dd/src/helper.php';
 
+
 use App\Application\Router\Route;
 use App\Application\Router\Router;
-use App\Controllers\PagesController;
 
 
-Route::page('/home', PagesController::class, 'home');
-Route::page('/about', PagesController::class, 'about');
-Route::page('/contacts', PagesController::class, 'contacts');
+
+require_once __DIR__ . '/routes/pages.php';
 
 
 $router = new Router();
