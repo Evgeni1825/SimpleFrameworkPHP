3e6a290a627a937755dddd3e8414930f0fd625e6
Evgeni1825 <44087970+Evgeni1825@users.noreply.github.com>
2023-10-23 16:49:27 +0300

    App-

---
+++ app/Application/App.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Application;
+
+use App\Application\Router\Route;
+use App\Application\Router\Router;
+
+class App
+{
+    public function run(): void
+    {
+        require_once __DIR__ . '/../../routes/pages.php';
+        $router = new Router();
+        $router->handle(Route::list());
+    }
+}
--- index.php
+++ index.php
@@ -3,17 +3,13 @@
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/vendor/larapack/dd/src/helper.php';
 
-
-use App\Application\Router\Route;
-use App\Application\Router\Router;
+use App\Application\App;
 
 
+$app = new App();
+$app->run();
 
-require_once __DIR__ . '/routes/pages.php';
 
-
-$router = new Router();
-$router->handle(Route::list());
 
 
 
