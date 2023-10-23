af0e85a31120742607403990fb64a4404d3730ce
Evgeni1825 <44087970+Evgeni1825@users.noreply.github.com>
2023-10-23 15:29:36 +0300

    CreatingRoutesAndRouter

---
+++ app/Application/Router/Router.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Application\Router;
+
+class Router implements RouterInterface
+{
+    public function handle(array $routes): void
+    {
+        $uri = $_SERVER['REQUEST_URI'];
+
+        foreach($routes as $route){
+            if ($route['uri'] === $uri){
+                echo $route['html'];
+            }
+        }
+    }
+}
--- index.php
+++ index.php
@@ -3,3 +3,15 @@
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/vendor/larapack/dd/src/helper.php';
 
+use App\Application\Router\Route;
+use App\Application\Router\Router;
+
+Route::page('/home', '<h2>Home page</h2>');
+Route::page('/about', '<h2>About page</h2>');
+Route::page('/contacts', '<h2>Contacts page</h2>');
+
+$router = new Router();
+$router->handle(Route::list());
+
+
+
