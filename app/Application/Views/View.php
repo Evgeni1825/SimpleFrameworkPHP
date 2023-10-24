93763c796de61f9466b427f8f926527eedda4912
Evgeni1825 <44087970+Evgeni1825@users.noreply.github.com>
2023-10-24 08:42:31 +0300

    Checking_file_exist_in_Views

--- app/Application/Views/View.php
+++ app/Application/Views/View.php
@@ -7,6 +7,9 @@ class View implements ViewInterface
     public static function show(string $view) :void
     {
         $path = __DIR__ . "/../../../views/$view.view.php";
+        if (!file_exists($path)){
+            throw new \Exception("View $view not found.");
+        }
         include $path;
     }
 }
