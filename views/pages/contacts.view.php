67938d394e0c2c57d5f40a521b60674a9d4a7888
Evgeni1825 <44087970+Evgeni1825@users.noreply.github.com>
2023-10-25 09:13:18 +0300

    Add_form_in_contacts.view

--- views/pages/contacts.view.php
+++ views/pages/contacts.view.php
@@ -13,7 +13,21 @@
     <?php View::component('nav'); ?>
         <div class="container">
             <div class="row mt-3">
-                <h2>Welcome to <span class="badge bg-secondary">Contacts</span></h2>
+                <form action="#" class="form" method="post">
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Subject</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" >
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Send</button>
+                </form>
             </div>
         </div>
     </main>
