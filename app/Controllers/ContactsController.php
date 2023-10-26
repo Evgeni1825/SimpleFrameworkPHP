f19eac6cedc01bcc2000061ca197acb582951bb3
Evgeni1825 <44087970+Evgeni1825@users.noreply.github.com>
2023-10-26 15:31:57 +0300

    Modify_ContactsController_Request

--- app/Controllers/ContactsController.php
+++ app/Controllers/ContactsController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controllers;
 use App\Application\Database\Connection;
+use App\Application\Request\Request;
 use App\Application\Router\Redirect;
 use App\Models\Report;
 
 class ContactsController
 {
-    public function submit(array $data) :void
+    public function submit(Request $request) :void
     {
         $report = new Report();
-        $report->setEmail($data['email']);
-        $report->setSubject($data['subject']);
-        $report->setMessage($data['message']);
+        $report->setEmail($request->post('email'));
+        $report->setSubject($request->post('subject'));
+        $report->setMessage($request->post('message'));
         $report->store();
         Redirect::to('/contacts');
     }
