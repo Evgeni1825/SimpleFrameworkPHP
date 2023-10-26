532ceb2053ebfa5719f99621fd374506a16afafe
Evgeni1825 <44087970+Evgeni1825@users.noreply.github.com>
2023-10-26 14:29:05 +0300

    Create_table_users_in_database_And_Model_User

---
+++ app/Models/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+use App\Application\Database\Model;
+
+class User extends Model
+{
+    protected array $fields = ['email', 'name', 'password'];
+    protected string $table = 'users';
+    protected string $email;
+    protected string $name;
+    protected string $password;
+
+    public function  setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function  setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function  setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+}
